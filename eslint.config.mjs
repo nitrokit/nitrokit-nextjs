fdf5950306359e98e68a7fcfc378e83a4abd827e
Mustafa Genc <eposta@mustafagenc.info>
2025-09-21 21:54:48 +0300

    Configures authentication with Prisma
    Sets up NextAuth with Prisma adapter for user authentication.
    
    Adds dependencies and scripts for Prisma schema management.
    
    Removes the unnecessary .npmrc file.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,70 +12,80 @@ const compat = new FlatCompat({
     baseDirectory: __dirname,
 });
 
-const eslintConfig = [{
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, {
-    ignores: [
-        'node_modules/**',
-        '.next/**',
-        'coverage/**',
-        'dist/**',
-        'build/**',
-        'stories/**',
-        '**/*.stories.{ts,tsx}',
-        'messages/**',
-        '**/*.json'
-    ],
-}, ...compat.extends('next/core-web-vitals'), {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-        parser: eslintParserTypescript,
-        parserOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            ecmaFeatures: {
-                jsx: true,
+const eslintConfig = [
+    {
+        ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    },
+    {
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'coverage/**',
+            'dist/**',
+            'build/**',
+            'stories/**',
+            'src/generated/**',
+            '**/*.stories.{ts,tsx}',
+            'messages/**',
+            '**/*.json',
+        ],
+    },
+    ...compat.extends('next/core-web-vitals'),
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: eslintParserTypescript,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
             },
         },
+        plugins: {
+            '@typescript-eslint': eslintPluginTypescript,
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            quotes: ['error', 'single', { avoidEscape: true }],
+            semi: ['error', 'always'],
+            '@typescript-eslint/interface-name-prefix': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'prefer-const': 'error',
+            'react-hooks/exhaustive-deps': 'error',
+            'prettier/prettier': [
+                'error',
+                {
+                    singleQuote: true,
+                    semi: true,
+                    tabWidth: 4,
+                    trailingComma: 'es5',
+                    printWidth: 100,
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    endOfLine: 'lf',
+                },
+            ],
+        },
     },
-    plugins: {
-        '@typescript-eslint': eslintPluginTypescript,
-        'prettier': eslintPluginPrettier,
+    {
+        files: ['**/sanitization.ts'],
+        rules: {
+            quotes: 'off',
+            'prettier/prettier': 'off',
+        },
     },
-    rules: {
-        'quotes': ['error', 'single', { 'avoidEscape': true }],
-        'semi': ['error', 'always'],
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/ban-ts-comment': 'off',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            {
-                'argsIgnorePattern': '^_',
-                'varsIgnorePattern': '^_',
-            },
-        ],
-        'prefer-const': 'error',
-        'react-hooks/exhaustive-deps': 'error',
-        'prettier/prettier': ['error', {
-            'singleQuote': true,
-            'semi': true,
-            'tabWidth': 4,
-            'trailingComma': 'es5',
-            'printWidth': 100,
-            'bracketSpacing': true,
-            'arrowParens': 'always',
-            'endOfLine': 'lf'
-        }],
-    },
-}, {
-    files: ['**/sanitization.ts'],
-    rules: {
-        'quotes': 'off',
-        'prettier/prettier': 'off'
-    }
-}];
+];
 
 export default eslintConfig;
