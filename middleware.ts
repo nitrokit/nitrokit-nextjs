a20a3c66abdaccee8d1e80ad2af95f5cca2f1f82
Mustafa Genc <eposta@mustafagenc.info>
2025-09-21 22:15:01 +0300

    Enhances application security and UI
    Adds rate limiting to API endpoints to protect against abuse.
    
    Introduces new font files and locale configurations to enhance the user interface.
    
    Removes unused middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,5 @@
 export { auth as middleware } from './src/lib/auth';
+
+export const config = {
+    matcher: ['/api/(.*)', '/((?!api|trpc|_next|_vercel|sitemap|robots|issues|.*\\..*).*)'],
+};
---
+++ src/constants/locale.ts
@@ -0,0 +1,46 @@
+import { getBaseUrl } from '@/lib';
+
+const DEFAULT_LANGUAGE = 'en';
+const DEFAULT_CURRENCY = 'USD';
+
+/**
+ * List of locales supported by the application.
+ * This is used to determine the language of the application.
+ * The default locale is Turkish (tr).
+ * The English locale is also supported (en).
+ * The application will use the browser's language settings to determine the default locale.
+ * @returns {string[]} An array of supported locales.
+ */
+const locales = ['en', 'tr'];
+
+/**
+ * List of locales with their respective flags.
+ * This is used to display the language selection in the UI.
+ * The flag URLs are relative to the public directory.
+ * For example, the flag for Turkish is located at /public/flags/tr.svg.
+ * The flag for English is located at /public/flags/us.svg.
+ * https://github.com/lipis/flag-icons
+ * @returns {Array<{ id: string, name: string, flag: string }>}
+ */
+const localesWithFlag = [
+    { id: 'en', name: 'English', flag: '/images/flags/us.svg' },
+    { id: 'tr', name: 'Türkçe', flag: '/images/flags/tr.svg' },
+];
+
+/**
+ * This function returns the locales for metadata.
+ * This is used to generate the metadata for the application.
+ * The metadata is used to determine the language of the application.
+ * The metadata is used in the middleware to redirect to the correct locale.
+ * @returns {Array<{ [key: string]: string }>} An array of objects with the locale and its URL.
+ */
+function localesForMetadata(): { [key: string]: string }[] {
+    const baseUrl = getBaseUrl();
+    return locales.map((locale) => {
+        return {
+            [locale]: `${baseUrl}/${locale}`,
+        };
+    });
+}
+
+export { locales, localesWithFlag, localesForMetadata, DEFAULT_LANGUAGE, DEFAULT_CURRENCY };
