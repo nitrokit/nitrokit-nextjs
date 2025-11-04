80c44c19317e462e1d75c291e726eff8b553f4e6
Mustafa Genc <eposta@mustafagenc.info>
2025-11-04 03:21:14 +0300

    feat: Adds app sidebar
    Adds a new sidebar component with main navigation.
    
    The sidebar includes menu items and styles the component.
    It's designed to fit within the header and footer.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -240,7 +240,7 @@ const Sidebar = React.forwardRef<
                             : 'right-3 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
                         // Adjust the padding for floating and inset variants.
                         variant === 'floating' || variant === 'inset'
-                            ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
+                            ? 'rounded-xl p-3 shadow-xs/10 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(3)))]'
                             : 'group-data-[collapsible=icon]:w-[--sidebar-width-icon] group-data-[side=left]:border-r group-data-[side=right]:border-l',
                         className
                     )}
@@ -633,7 +633,6 @@ const SidebarMenuSkeleton = React.forwardRef<
         showIcon?: boolean;
     }
 >(({ className, showIcon = false, ...props }, ref) => {
-    // Random width between 50 to 90%.
     const width = React.useMemo(() => {
         return `${Math.floor(Math.random() * 40) + 50}%`;
     }, []);
